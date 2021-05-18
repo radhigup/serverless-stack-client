c55f7d208277ad1323a67e26ed348d5b3ee4d714
Gupta <radhigup@3c22fb06f051.ant.amazon.com>
2021-05-18 14:43:24 -0400

    setting up react app

---
+++ src/config.js
@@ -0,0 +1,18 @@
+const config = {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "notes-app-upload321",
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://etizbo9qa0.execute-api.us-east-1.amazonaws.com/prod",
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_mJUw3ZWA3",
+      APP_CLIENT_ID: "62obi2e3fjn1asqlhoptld0re0",
+      IDENTITY_POOL_ID: "us-east-1:35120c22-3b2a-4606-9a8a-5f9019a851f5",
+    },
+  };
+  
+  export default config;
